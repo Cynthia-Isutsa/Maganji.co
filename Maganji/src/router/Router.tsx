931b81fcd350c9199ac4f297bc877660c6de0b9f
Cynthia-Isutsa <isutsacynthia@gmail.com>
2023-10-21 01:40:20 +0300

    Routes

--- Maganji/src/router/Router.tsx
+++ Maganji/src/router/Router.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import App from "../App"
 
 const Router = () => {
   return (
     <div>
-      
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<App />}>
+
+                </Route>
+            </Routes>
+        </BrowserRouter>
     </div>
   )
 }
