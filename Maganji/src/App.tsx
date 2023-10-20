4b3b4afa679de872b6fa3d718529efd2ca116f04
Cynthia-Isutsa <isutsacynthia@gmail.com>
2023-10-21 01:31:46 +0300

    Dashboard

--- Maganji/src/App.tsx
+++ Maganji/src/App.tsx
@@ -1,13 +1,19 @@
 
 import Dashboardview from "./components/Dashboardview"
 import Sidebar from "./components/Sidebar"
-
+import {Outlet} from 'react-router-dom'
 
 const App = () => {
   return (
     <div className="flex h-[100vh]">
       <div className='basis-[12%] border'><Sidebar /></div>
-      <div className='basis-[88%] border'><Dashboardview /></div>
+      <div className='basis-[88%] border'>
+        <Dashboardview />
+        <div>
+          <Outlet></Outlet>
+        </div>
+
+      </div>
 
     </div>
   )
--- Maganji/src/components/Dashboardview.tsx
+++ Maganji/src/components/Dashboardview.tsx
@@ -1,14 +1,29 @@
-import { FaSearch } from "react-icons/fa"
+import { FaRegBell, FaRegEnvelope, FaSearch } from "react-icons/fa"
+import image1 from '../assets/image1.jpg'
+
 
 
 const Dashboardview = () => {
   return (
-    <div>
+    <div className='py-2 flex items-center justify-between h-[70px] shadow-lg px-[25px]'>
       <div className='flex items-center rounded-lg'>
-        <input type="text" />
-        <div>
-            <FaSearch />
+        <input type="text" placeholder='Search for....' className='bg-gray-300 h-[40px] outline-none pl-4 w-[350px] rounded-sm placeholder:text-xl font-normal'/>
+        <div className='bg-[#4E73DF] h-[40px] px-3 flex items-center justify-center cursor-pointer rounded-sm '>
+            <FaSearch size={22} className= 'fill-white'/>
         </div>
+      </div>
+
+      <div className='flex items-center gap-[15px] relative'>
+        <div className="flex items-center gap-[25px] border-r-[1px] border-red-700 pr-[25px]">
+          <FaRegBell  />
+          <FaRegEnvelope />
+        </div>
+        <div className='flex items-center gap-5 relative '>
+        <p>Cynthia Isutsa</p>
+        <div className="h-[50px] w-[50px] rounded-full cursor-pointer items-center justify-center relative">
+          <img src={image1} alt='profile' />
+        </div>
+      </div>
       </div>
     </div>
   )
